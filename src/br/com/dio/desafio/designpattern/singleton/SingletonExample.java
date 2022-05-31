3339ff5ca19c3bb251e93dc9363f76513194b73c
Raphael <raphael.otaviano17@gmail.com>
2022-05-31 01:54:44 -0300

    Criando um exemplo para Singleton

--- src/br/com/dio/desafio/designpattern/singleton/SingletonExample.java
+++ src/br/com/dio/desafio/designpattern/singleton/SingletonExample.java
@@ -4,10 +4,25 @@ package br.com.dio.desafio.designpattern.singleton;
 public class SingletonExample {
 
 
+    private static void getInstanceLazy() {
+        SingletonInterface singleton = SingletonLazy.getInstance();
+        System.out.println("Instância Singleton Lazy: "+singleton);
+    }
+
+    private static void getInstanceEager() {
+        SingletonInterface singleton = SingletonEager.getInstance();
+        System.out.println("Instância Singleton Eager: "+singleton);
+    }
+
+
+    private static void getInstanceLazyHolder() {
+        SingletonInterface singleton = SingletonLazyHolder.getInstance();
+        System.out.println("Instância Singleton  Holder: "+singleton);
+    }
 
     public static void main(String[] args) {
-        //SingletonExample.getInstanceLazy();
-        //SingletonExample.getInstanceEager();
-        //SingletonExample.getInstanceLazyHolder();
+        SingletonExample.getInstanceLazy();
+        SingletonExample.getInstanceEager();
+        SingletonExample.getInstanceLazyHolder();
     }
 }
